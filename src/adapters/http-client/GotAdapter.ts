aac85408dd9deac5d86297bbca522f61ffd8a661
lucsimao <lucasimao64@gmail.com>
2022-03-29 20:37:44 -0300

    feat: create GotAdapter

---
+++ src/adapters/http-client/GotAdapter.ts
@@ -0,0 +1,30 @@
+import got, { OptionsOfJSONResponseBody } from 'got';
+
+import { HttpClient, HttpOptions, HttpResponse } from '../../interfaces';
+
+export class GotAdapter implements HttpClient {
+  async get<T>(url: string, options?: HttpOptions): Promise<HttpResponse<T>> {
+    const gotOptions: OptionsOfJSONResponseBody = {
+      responseType: 'json',
+      ...(options as OptionsOfJSONResponseBody),
+    };
+    const result = await got.get<T>(url, gotOptions);
+
+    return { statusCode: result.statusCode, body: result.body as T };
+  }
+
+  public async post<T, K>(
+    url: string,
+    options?: HttpOptions,
+  ): Promise<HttpResponse<K>> {
+    const gotOptions: OptionsOfJSONResponseBody = {
+      responseType: 'json',
+      json: options?.body as Record<string, T>,
+      headers: options?.headers,
+    };
+
+    const result = await got.post<K>(url, gotOptions);
+
+    return { statusCode: result.statusCode, body: result.body as K };
+  }
+}

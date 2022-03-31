a3c90c2d0b923ef0318cd808e8eb1811206f0ba6
Bruno Afonso <brunohafonso@gmail.com>
2022-03-30 22:39:35 -0300

    improvement: adjust the express adapter to accept custom methods
    add to the express adapter the capability to accept the method that we
    need to invoke from the controller.
    
    closes #10

--- src/adapters/express/ExpressRouteAdapter.ts
+++ src/adapters/express/ExpressRouteAdapter.ts
@@ -1,24 +1,54 @@
 import { NextFunction, Request, Response } from 'express';
+import { StatusCodes } from 'http-status-codes';
+import { IHttpFormatedResponse } from 'src/interfaces/http/FormatedApiHttpResponse';
 
-import { Controller } from '../../interfaces/controllers';
+import { ApiHttpRequest, ApiHttpResponse } from '../../interfaces/http';
 
 export class ExpressRouteAdapter {
-  public static adapt<T, K>(controller: Controller<T, K>) {
+  public static adapt<
+    Controller = {
+      [key: string]: (httpRequest: ApiHttpRequest) => Promise<ApiHttpResponse>;
+    },
+  >(
+    controller: {
+      [key in keyof Controller]: (
+        httpRequest: ApiHttpRequest,
+      ) => Promise<ApiHttpResponse>;
+    },
+    methodName: keyof Controller,
+  ) {
     return async (
       req: Request,
       res: Response,
       next: NextFunction,
-    ): Promise<void> => {
+    ): Promise<Response | void> => {
       try {
-        const request = {
+        const httpResponse: ApiHttpResponse = await controller[methodName]({
           body: req.body,
-        };
-        const httpResponse = await controller.handle(request);
-
-        res.status(httpResponse.statusCode).json(httpResponse.body);
+          headers: req.headers,
+          params: req.params,
+          query: req.query,
+        });
+        ExpressRouteAdapter.setResponseHeaders(httpResponse, res);
+        return res.status(StatusCodes[httpResponse.statusCodeAsString]).json({
+          statusCode: StatusCodes[httpResponse.statusCodeAsString],
+          statusCodeAsString: httpResponse.statusCodeAsString,
+          data: httpResponse.body,
+        } as IHttpFormatedResponse);
       } catch (error) {
-        next(error);
+        return next(error);
       }
     };
+  }
+
+  private static setResponseHeaders(
+    response: ApiHttpResponse,
+    res: Response,
+  ): void {
+    if (response.headers) {
+      Object.entries(response.headers).forEach(([key, value]) =>
+        res.set(key, value),
+      );
+    }
   }
 }

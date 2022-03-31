4108f6c6cece6d564a4ff8c65a3146c98aa26cce
Bruno Afonso <brunohafonso@gmail.com>
2022-03-30 22:56:48 -0300

    ci: add new rules to commitlint to check commit body and footer
    add new rules to commitlint to check commit body and footer
    
    refs #1

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,20 +1,26 @@
 module.exports = {
-    extends: ['@commitlint/config-conventional'], 'type-enum': [
-        2,
-        'always',
-        [
-            'build',
-            'chore',
-            'improvement',
-            'ci',
-            'docs',
-            'feat',
-            'fix',
-            'perf',
-            'refactor',
-            'revert',
-            'style',
-            'test',
+    extends: ['@commitlint/config-conventional'],
+    rules: {
+
+        'type-enum': [
+            2,
+            'always',
+            [
+                'build',
+                'chore',
+                'improvement',
+                'ci',
+                'docs',
+                'feat',
+                'fix',
+                'perf',
+                'refactor',
+                'revert',
+                'style',
+                'test',
+            ],
         ],
-    ],
+        'footer-empty': [2, 'never'],
+        'body-empty': [2, 'never'],
+    },
 };

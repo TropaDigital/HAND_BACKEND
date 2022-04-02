65a6fa9087cc2f13a173d503dc39c8742bb9a219
Bruno Afonso <brunohafonso@gmail.com>
2022-04-02 00:47:18 -0300

    refactor: adjust pino adapter to receive a pino instance as default constructor parameter
    adjust pino adapter to receive a pino instance as default constructor parameter
    
    refs #11

--- src/factories/LoggerFactory.ts
+++ src/factories/LoggerFactory.ts
@@ -1,9 +1,9 @@
-import PinoAdapter from '../adapters/logger/PinoAdapter';
+import { PinoAdapter } from '../adapters/logger/PinoAdapter';
 import { LoggerManager } from '../services/logger/LoggerManager';
 
 export class LoggerFactory {
   public static create(): LoggerManager {
     const pinoAdapter = new PinoAdapter();
-    return new LoggerManager(pinoAdapter);
+    return new LoggerManager([pinoAdapter]);
   }
 }

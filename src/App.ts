ddfa7c38280552cf2b664e6627c33ae5a9b33d81
Bruno Afonso <brunohafonso@gmail.com>
2022-04-05 00:19:21 -0300

    feat: add swagger and swagger stats config in the project
    add swagger and swagger stats config in the project
    
    closes #4

--- src/App.ts
+++ src/App.ts
@@ -1,13 +1,17 @@
 import cors from 'cors';
 import express, { Application } from 'express';
 import helmet from 'helmet';
-import { createServer, Server } from 'http';
+import { createServer, IncomingMessage, Server } from 'http';
+import openapi from 'openapi-comment-parser';
+import swaggerStats from 'swagger-stats';
+import swaggerUI from 'swagger-ui-express';
 
 import { LoggerFactory } from './factories/LoggerFactory';
 import MySqlDBClient from './infra/mySql';
 import { ILogger } from './interfaces/logger/Logger';
 import { errorMiddleware, loggerMiddleware } from './middlewares';
 import { healthcheckRouter } from './modules';
+import openapiConfig from './openapirc';
 
 export default class App {
   private application: Application;
@@ -25,32 +29,32 @@ export default class App {
       this.logger.info({ msg: 'skiping setup of swagger docs' });
     }
 
-    // this.logger.info({ msg: 'setuping swagger docs' });
-    // const apiSchema = openapi(openapiConfig);
-    // this.application.use('/docs', swaggerUI.serve, swaggerUI.setup(apiSchema));
+    this.logger.info({ msg: 'setuping swagger docs' });
+    const apiSchema = openapi(openapiConfig);
+    this.application.use('/docs', swaggerUI.serve, swaggerUI.setup(apiSchema));
   }
 
   private setupSwaggerStats(): void {
     this.logger.info({ msg: 'setuping swagger stats' });
-    // const apiSchema = openapi(openapiConfig);
-    // this.application.use(
-    //   swaggerStats.getMiddleware({
-    //     swaggerSpec: apiSchema,
-    //     uriPath: '/monitoring',
-    //     authentication: true,
-    //     swaggerOnly: true,
-    //     onAuthenticate(
-    //       _req: IncomingMessage,
-    //       username: string,
-    //       password: string,
-    //     ) {
-    //       return (
-    //         username === swaggerStatsConfig.SWAGGER_STATS_USER &&
-    //         password === swaggerStatsConfig.SWAGGER_STATS_PASSWORD
-    //       );
-    //     },
-    //   }),
-    // );
+    const apiSchema = openapi(openapiConfig);
+    this.application.use(
+      swaggerStats.getMiddleware({
+        swaggerSpec: apiSchema,
+        uriPath: '/monitoring',
+        authentication: true,
+        swaggerOnly: true,
+        onAuthenticate(
+          _req: IncomingMessage,
+          username: string,
+          password: string,
+        ) {
+          return (
+            username === 'SWAGGER_STATS_USER' &&
+            password === 'SWAGGER_STATS_PASSWORD'
+          );
+        },
+      }),
+    );
   }
 
   private setupGlobalErrorMiddlewares(): void {
--- src/modules/healthcheck/routes.ts
+++ src/modules/healthcheck/routes.ts
@@ -24,7 +24,16 @@ export default class HealthCheckRouter implements IRouter {
 
   private checkApplicationStatus(): void {
     /**
-     *
+     * GET /healthcheck
+     * @tag Healthcheck
+     * @summary get the status of the application considering the status of all the required resources.
+     * @description return an object with the status of the resources.
+     * @response 200 - an object with the status of the resources when all is healthly.
+     * @responseContent {HealthcheckResponse} 200.application/json
+     * @responseExample {HealthlyResponse} 200.application/json.HealthlyResponse
+     * @response 500 - an object with the status of the resources when all is healthly.
+     * @responseContent {HealthcheckResponse} 500.application/json
+     * @responseExample {UnhealthlyResponse} 500.application/json.UnhealthlyResponse
      */
     this.router
       .route('/healthcheck')

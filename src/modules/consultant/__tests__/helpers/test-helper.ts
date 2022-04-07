d566e85cdbe648555e739c5101a2b8db3b096b1c
lucsimao <lucasimao64@gmail.com>
2022-04-06 22:47:01 -0300

    test(consultant): create test helper
    create test helper
    
    refs #32

---
+++ src/modules/consultant/__tests__/helpers/test-helper.ts
@@ -0,0 +1,76 @@
+import { Consultant, Prisma } from '@prisma/client';
+import { StatusCodes } from 'http-status-codes';
+
+import { IApiHttpRequest, IApiHttpResponse } from '../../../../interfaces/http';
+import { IValidator } from '../../../../interfaces/validation/IValidator';
+import { IConsultantService } from '../../interfaces';
+
+export const makeFakeCreateConsultantInput =
+  (): jest.Mocked<Prisma.ConsultantCreateInput> => ({
+    name: 'any_name',
+    taxId: '00000000000',
+    city: 'any_city',
+    state: 'any_state',
+    commission: 10,
+    createdBy: 'any_user',
+    updatedBy: 'any_date',
+    createdAt: new Date(),
+    updatedAt: new Date(),
+    deletedAt: new Date(),
+  });
+
+export const makeFakeUpdateConsultantInput =
+  (): jest.Mocked<Prisma.ConsultantUpdateInput> =>
+    makeFakeCreateConsultantInput();
+
+export const makeFakeApiHttpRequest = ({
+  body,
+  params,
+  headers,
+}: {
+  body?: unknown;
+  params?: { [key: string]: any };
+  headers?: { [key: string]: string | string[] | undefined };
+}): jest.Mocked<IApiHttpRequest> => ({ body, params, headers });
+
+export const makeFakeApiHttpResponse = (
+  status: keyof typeof StatusCodes,
+  body?: unknown,
+): jest.Mocked<IApiHttpResponse> => ({
+  body,
+  statusCodeAsString: status || 'OK',
+});
+
+export const makeFakeConsultant = (
+  name = 'any_name',
+): jest.Mocked<Consultant> => ({
+  id: 777,
+  name,
+  taxId: '00000000000',
+  city: 'any_city',
+  state: 'any_state',
+  commission: 10,
+  createdBy: 'any_user',
+  updatedBy: 'any_date',
+  createdAt: new Date(),
+  updatedAt: new Date(),
+  deletedAt: new Date(),
+});
+
+export const makeFakeConsultantList = () => [
+  makeFakeConsultant('any_name1'),
+  makeFakeConsultant('any_name2'),
+];
+
+export const makeConsultantServiceStub =
+  (): jest.Mocked<IConsultantService> => ({
+    getAllConsultants: jest.fn().mockResolvedValue(makeFakeConsultantList()),
+    getConsultantById: jest.fn().mockResolvedValue(makeFakeConsultant()),
+    createConsultant: jest.fn().mockResolvedValue(makeFakeConsultant()),
+    updateConsultant: jest.fn(),
+    deleteConsultant: jest.fn(),
+  });
+
+export const makeValidatorStub = (): jest.Mocked<IValidator> => ({
+  validateSchema: jest.fn().mockReturnValue({ id: 777 }),
+});

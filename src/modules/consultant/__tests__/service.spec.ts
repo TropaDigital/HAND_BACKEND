4a1bdd2a5eecb6042a6b3fb4b1340dd07c1a6391
lucsimao <lucasimao64@gmail.com>
2022-04-06 22:49:27 -0300

    test(consultant): create consultant service tests
    create consultant service tests
    
    refs #32

---
+++ src/modules/consultant/__tests__/service.spec.ts
@@ -0,0 +1,171 @@
+import { IConsultantRepository } from '../interfaces';
+import { ConsultantService } from '../service';
+import {
+  makeFakeConsultant,
+  makeFakeConsultantList,
+  makeFakeCreateConsultantInput,
+  makeFakeUpdateConsultantInput,
+} from './helpers/test-helper';
+
+jest.useFakeTimers().setSystemTime(new Date('1912-06-23').getTime());
+
+const makeConsultantRepositoryStub =
+  (): jest.Mocked<IConsultantRepository> => ({
+    findAll: jest.fn().mockResolvedValue(makeFakeConsultantList()),
+    findById: jest.fn().mockResolvedValue(makeFakeConsultant()),
+    create: jest.fn().mockResolvedValue(makeFakeConsultant()),
+    update: jest.fn(),
+    delete: jest.fn(),
+  });
+
+const makeSut = () => {
+  const consultantRepository = makeConsultantRepositoryStub();
+  const sut = new ConsultantService(consultantRepository);
+
+  return { sut, consultantRepository };
+};
+
+describe(ConsultantService.name, () => {
+  describe(`When ${ConsultantService.prototype.getAllConsultants.name} is called`, () => {
+    it('should call repository with right params', async () => {
+      const { sut, consultantRepository } = makeSut();
+      const findAllSpy = consultantRepository.findAll;
+
+      await sut.getAllConsultants();
+
+      expect(findAllSpy).toBeCalledWith();
+    });
+
+    it('should return repository result', async () => {
+      const { sut } = makeSut();
+
+      const result = await sut.getAllConsultants();
+
+      expect(result).toEqual(makeFakeConsultantList());
+    });
+
+    it('should throw when repository throws', async () => {
+      const { sut, consultantRepository } = makeSut();
+      consultantRepository.findAll.mockRejectedValueOnce(
+        new Error('any_find_all_error'),
+      );
+
+      const promise = sut.getAllConsultants();
+
+      await expect(promise).rejects.toThrow(new Error('any_find_all_error'));
+    });
+  });
+
+  describe(`When ${ConsultantService.prototype.getConsultantById.name} is called`, () => {
+    const fakeId = 777;
+
+    it('should call repository with right params', async () => {
+      const { sut, consultantRepository } = makeSut();
+      const findByIdSpy = consultantRepository.findById;
+
+      await sut.getConsultantById(fakeId);
+
+      expect(findByIdSpy).toBeCalledWith(777);
+    });
+
+    it('should return repository result', async () => {
+      const { sut } = makeSut();
+
+      const result = await sut.getConsultantById(fakeId);
+
+      expect(result).toEqual(makeFakeConsultant());
+    });
+
+    it('should throw when repository throws', async () => {
+      const { sut, consultantRepository } = makeSut();
+      consultantRepository.findById.mockRejectedValueOnce(
+        new Error('any_find_by_error'),
+      );
+
+      const promise = sut.getConsultantById(fakeId);
+
+      await expect(promise).rejects.toThrow(new Error('any_find_by_error'));
+    });
+  });
+
+  describe(`When ${ConsultantService.prototype.createConsultant.name} is called`, () => {
+    const fakeConsultant = makeFakeCreateConsultantInput();
+
+    it('should call repository with right params', async () => {
+      const { sut, consultantRepository } = makeSut();
+      const createSpy = consultantRepository.create;
+
+      await sut.createConsultant(fakeConsultant);
+
+      expect(createSpy).toBeCalledWith(makeFakeCreateConsultantInput());
+    });
+
+    it('should return repository result', async () => {
+      const { sut } = makeSut();
+
+      const result = await sut.createConsultant(fakeConsultant);
+
+      expect(result).toEqual(makeFakeConsultant());
+    });
+
+    it('should throw when repository throws', async () => {
+      const { sut, consultantRepository } = makeSut();
+      consultantRepository.create.mockRejectedValueOnce(
+        new Error('any_create_error'),
+      );
+
+      const promise = sut.createConsultant(fakeConsultant);
+
+      await expect(promise).rejects.toThrow(new Error('any_create_error'));
+    });
+  });
+
+  describe(`When ${ConsultantService.prototype.updateConsultant.name} is called`, () => {
+    const fakeId = 777;
+    const fakeConsultant = makeFakeUpdateConsultantInput();
+
+    it('should call repository with right params', async () => {
+      const { sut, consultantRepository } = makeSut();
+      const updateSpy = consultantRepository.update;
+
+      await sut.updateConsultant(fakeId, fakeConsultant);
+
+      expect(updateSpy).toBeCalledWith(777, makeFakeUpdateConsultantInput());
+    });
+
+    it('should throw when repository throws', async () => {
+      const { sut, consultantRepository } = makeSut();
+      consultantRepository.update.mockRejectedValueOnce(
+        new Error('any_update_error'),
+      );
+
+      const promise = sut.updateConsultant(fakeId, fakeConsultant);
+
+      await expect(promise).rejects.toThrow(new Error('any_update_error'));
+    });
+  });
+
+  describe(`When ${ConsultantService.prototype.deleteConsultant.name} is called`, () => {
+    const fakeId = 777;
+
+    it('should call repository with right params', async () => {
+      const { sut, consultantRepository } = makeSut();
+      const deleteSpy = consultantRepository.delete;
+
+      await sut.deleteConsultant(fakeId);
+
+      expect(deleteSpy).toBeCalledWith(777);
+    });
+
+    it('should throw when repository throws', async () => {
+      const { sut, consultantRepository } = makeSut();
+      consultantRepository.delete.mockRejectedValueOnce(
+        new Error('any_delete_error'),
+      );
+
+      const promise = sut.deleteConsultant(fakeId);
+
+      await expect(promise).rejects.toThrow(new Error('any_delete_error'));
+    });
+  });
+});

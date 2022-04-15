8a640fb8c2912b93670fca66ba3ffee5a442a6a3
Bruno Afonso <brunohafonso@gmail.com>
2022-04-14 23:19:45 -0300

    test: add integration tests of global middlewares

---
+++ __tests__/functionals/middlewares/index.test.ts
@@ -0,0 +1,31 @@
+/* eslint-disable max-nested-callbacks */
+import { HealthcheckController } from '../../../src/modules/healthcheck/controller';
+import { NotFoundError } from '../../../src/shared/errors';
+import { makeNotFoundResponse } from '../../helpers';
+
+describe('test global error middleware integration', () => {
+  describe('GET /route-that-does-not-exists', () => {
+    it('should return an not found object', async () => {
+      const response = await global.testRequest.get(
+        '/route-that-does-not-exists',
+      );
+
+      const expectedResult = makeNotFoundResponse();
+      expect(response.body).toEqual(expectedResult);
+      expect(response.status).toBe(expectedResult.statusCode);
+    });
+  });
+
+  describe('GET /healthcheck', () => {
+    it('should return an not found object', async () => {
+      jest
+        .spyOn(HealthcheckController.prototype, 'getApplicationStatus')
+        .mockRejectedValueOnce(new NotFoundError(''));
+      const response = await global.testRequest.get('/healthcheck');
+
+      const expectedResult = makeNotFoundResponse('');
+      expect(response.body).toEqual(expectedResult);
+      expect(response.status).toBe(expectedResult.statusCode);
+    });
+  });
+});

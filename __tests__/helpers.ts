0c2167b47179b8e167d7b330e76fc33940b69841
Bruno Afonso <brunohafonso@gmail.com>
2022-04-14 22:35:08 -0300

    test: refactor the consultant module integration tests
    refactor the consultant module integration tests
    
    refs #32

---
+++ __tests__/helpers.ts
@@ -0,0 +1,25 @@
+import { IFormatedError } from '../src/interfaces/errors/IFormatedError';
+import IValidationErrors from '../src/interfaces/validation/IValidationError';
+import { ErrorMiddleware } from '../src/middlewares/ErrorMiddleware';
+import {
+  NotFoundError,
+  MissingInvalidParamsError,
+  GenericAppError,
+} from '../src/shared/errors';
+
+export const makeNotFoundResponse = (description?: string): IFormatedError => {
+  return ErrorMiddleware.formatError(new NotFoundError(description));
+};
+
+export const makeInvalidParamsResponse = (
+  validationErrors: IValidationErrors[],
+): IFormatedError => {
+  return ErrorMiddleware.formatError(
+    new MissingInvalidParamsError(undefined, undefined, validationErrors),
+  );
+};
+
+export const makeInternalErrorResponse = (
+  description = 'Internal Server Error',
+): IFormatedError =>
+  ErrorMiddleware.formatError(new GenericAppError(description));

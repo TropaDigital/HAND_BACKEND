273a409b49805d784e6ec81005ea9b4f8596cffa
Bruno Afonso <brunohafonso@gmail.com>
2022-05-01 17:21:55 -0300

    test: adjust the tests of authmiddleware

--- __tests__/functionals/middlewares/authMiddleware.test.ts
+++ __tests__/functionals/middlewares/authMiddleware.test.ts
@@ -1,7 +1,9 @@
 /* eslint-disable max-nested-callbacks */
 
+import { StatusCodes } from 'http-status-codes';
 import { TokenExpiredError } from 'jsonwebtoken';
 
+import ErrorCodes from '../../../src/enums/ErrorCodes';
 import { AuthenticationService } from '../../../src/shared/auth/auth';
 import { makeUnauthorizedResponse } from '../../helpers';
 
@@ -17,7 +19,10 @@ describe('Auth middleware', () => {
         '/loansimulations/simulate',
       );
 
-      const expectedResult = makeUnauthorizedResponse('token not provided');
+      const expectedResult = makeUnauthorizedResponse(
+        'token not provided',
+        ErrorCodes.AUTH_ERROR_001,
+      );
       expect(response.body).toEqual(expectedResult);
       expect(response.status).toBe(expectedResult.statusCode);
     });
@@ -32,9 +37,13 @@ describe('Auth middleware', () => {
         .post('/loansimulations/simulate')
         .set('x-access-token', token);
 
-      const expectedResult = makeUnauthorizedResponse('User not authenticated');
-      expect(response.body).toEqual(expectedResult);
-      expect(response.status).toBe(expectedResult.statusCode);
+      expect(response.body).toEqual({
+        code: 'GENERIC_ERROR',
+        description: 'Internal Server Error',
+        statusCode: 500,
+        statusCodeAsString: 'INTERNAL_SERVER_ERROR',
+      });
+      expect(response.status).toBe(StatusCodes.INTERNAL_SERVER_ERROR);
     });
 
     it('should return an unauthorized error when the auth service throws an json web token error', async () => {

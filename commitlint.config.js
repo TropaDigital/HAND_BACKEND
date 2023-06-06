d9e57384425d0fcfb325e9543b1a04889af9f954
Bruno Afonso <brunohafonso@gmail.com>
2023-06-05 21:17:01 -0300

    ci: adjust the local ci to validate commit messages correctly

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,26 +1,23 @@
 module.exports = {
-    extends: ['@commitlint/config-conventional'],
-    rules: {
-
-        'type-enum': [
-            2,
-            'always',
-            [
-                'build',
-                'chore',
-                'improvement',
-                'ci',
-                'docs',
-                'feat',
-                'fix',
-                'perf',
-                'refactor',
-                'revert',
-                'style',
-                'test',
-            ],
-        ],
-        'footer-empty': [2, 'never'],
-        'body-empty': [2, 'never'],
-    },
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [
+      2,
+      'always',
+      [
+        'build',
+        'chore',
+        'improvement',
+        'ci',
+        'docs',
+        'feat',
+        'fix',
+        'perf',
+        'refactor',
+        'revert',
+        'style',
+        'test',
+      ],
+    ],
+  },
 };
